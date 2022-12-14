13c5120d07ac91fd8cdeb1254813017f454b7187
PranoySarker <pranoysarker01@gmail.com>
2022-12-14 15:46:06 +0600

    data get from api

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 const ProductComponent = () => {
+    const products = useSelector((state) => state.allProducts.products);
+
+    const productList = products.map((product) => {
+        const { id, title, image, price, category } = product;
+        return (
+            <div>
+                <div className='four column wide' key={id}>
+                    <div className='ui link cards'>
+                        <div className='card'>
+                            <div className='image' style={{ width: '15rem', height: '20rem' }}>
+                                <img src={image} alt="" />
+                            </div>
+                            <div className="content">
+                                <div className="header">{title}</div>
+                                <div className="meta price">$-{price}</div>
+                                <div className='meta'>{category}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    })
+
     return (
-        <div>
-            <h1>productComponent</h1>
-        </div>
+        <>
+            {productList}
+        </>
     );
 };
 
--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import ProductComponent from './ProductComponent';
+import axios from 'axios';
+import { setProducts } from '../redux/actions/productAction';
 
 const ProductListing = () => {
+    const products = useSelector((state) => state);
+    const dispatch = useDispatch();
+
+    const fetchProducts = async () => {
+        const response = await axios
+            .get('https://fakestoreapi.com/products')
+            .catch((err) => console.log("Err", err))
+        dispatch(setProducts(response.data));
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, [])
+    console.log("products :", products);
     return (
-        <div>
-            <h1>productListing</h1>
+        <div className='ui grid container hidden section divider'>
+            <ProductComponent></ProductComponent>
         </div>
     );
 };
